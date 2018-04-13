42b7a86fae405c1064a43aedb877d5587a7ca3ae
Ryan Blunden <ryan.blunden@gmail.com>
2018-04-13 15:27:00 -0700

    Added country_codes.csv and updated the weather challenge to include another bonus challenge of validating the user supplied country code.

--- code-challenges/weather_challenge.py
+++ code-challenges/weather_challenge.py
@@ -7,6 +7,7 @@
 #
 # BONUS:
 # -  Ask them what unit they would like the temperature in.
+# - Validate their choice of country code by loading in `data/country_codes.csv`.
 # - Supply some extra advice (e.g, if its raining, say ‘better bring your umbrella’)
 # - If you were to package this in a module for other users to use, you would not want to hard-code the `API key`. How could you make this configurable?
 # - Use the Python `click` library to learn about building scalable `CLI` applications.
